4b2c25345281d801eba241b98c823493e80755ed
ntorin <xtoribear@gmail.com>
2017-02-11 08:39:10 -0800

    fixed page titles, added hidden fields for Username and CreationDate, changed column display names

--- ZenithDataLib/Models/Activity.cs
+++ ZenithDataLib/Models/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace ZenithDataLib.Models
 {
     public class Activity
     {
+
+        [Display(Name = "Activity ID")]
         public int ActivityId { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Date Created")]
         public DateTime CreationDate { get; set; }
 
         public List<Event> Events { get; set; }
--- ZenithDataLib/Models/Event.cs
+++ ZenithDataLib/Models/Event.cs
@@ -10,13 +10,27 @@ namespace ZenithDataLib.Models
 {
     public class Event
     {
+
+        [Display(Name = "Event ID")]
         public int EventId { get; set; }
         // [DisplayFormat(DataFormatString = "{0:h:mm tt}")]
+
+        [Display(Name = "Starting Time")]
         public DateTime EventFrom { get; set; }
+
+        [Display(Name = "Ending time")]
         public DateTime EventTo { get; set; }
+
+        [Display(Name = "Creator")]
         public string EnteredBy { get; set; }
+
+        [Display(Name = "Activity ID")]
         public int ActivityId { get; set; }
+
+        [Display(Name = "Date Created")]
         public DateTime CreationDate { get; set; }
+
+        [Display(Name = "Active?")]
         public bool IsActive { get; set; }
     }
 }
